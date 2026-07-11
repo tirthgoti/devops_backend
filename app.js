3ac93e27ecad307442dac5befd9731897f8e8928
tirthgoti <tirthgoti8304@gmail.com>
2026-07-11 14:53:48 +0530

    changes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let express = require('express');
 let cors= require('cors');
-app.use(cors());
 let app =express();
 let port =9000;
+
+app.use(cors());
 
 app.get('/ping', (req,res)=>{
     res.send({
